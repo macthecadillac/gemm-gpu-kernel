24d58f7cbeadac4169f2bd40c3e91ec58e195935
Mac Lee <macthecadillac@gmail.com>
2026-07-22 23:45:07 +0800

    Changed tile size. Not correct

--- baseline.py
+++ baseline.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 n = 4096
-a = np.full((n, n), 2.)
-b = np.full((n, n), 3.)
+a = np.arange(0, 4096 * 4096).reshape(4096, -1) / (2048 * 2048)
+b = np.arange(4096 * 4096 - 1, -1, -1).reshape(4096, -1) / (2048 * 2048) - 0.5
 c = np.full((n, n), 0.1)
 alpha = 1.
 beta = 1.
@@ -14,3 +14,4 @@ c = alpha * a @ b + beta * c
 end = time.time()
 print(end - start)
 print(c[0, 0])
+print(c[345, 879])
--- gemm-rs/src/main.rs
+++ gemm-rs/src/main.rs
@@ -80,139 +80,61 @@ fn kernel<'a>(
     c: &'a mut Matrix,
     alpha: f64,
 ) {
-    //     Your current order is effectively:
-    //
-    // for i {
-    //     for k {
-    //         for j {
-    //             c[i][j] += a[i][k] * b[k][j];
-    //         }
-    //     }
-    // }
-    //
-    // Register blocking uses:
-    //
-    // for ii in (0..m).step_by(MR) {
-    //     for jj in (0..n).step_by(NR) {
-    //         // Load a small C tile into SIMD accumulators.
-    //
-    //         for k in 0..l {
-    //             // Load B[k, jj..jj+NR].
-    //             // Broadcast A[ii+r, k].
-    //             // FMA into every accumulator.
-    //         }
-    //
-    //         // Store the C tile once.
-    //     }
-    // }
-    //
-    // For NEON f64, one vector holds two doubles. A simple 2×4 microkernel would look conceptually like:
-    //
-    // // Two rows, four columns: four f64x2 accumulator registers.
-    // let mut c00 = load(&c[row0][col..col + 2]);
-    // let mut c01 = load(&c[row0][col + 2..col + 4]);
-    // let mut c10 = load(&c[row1][col..col + 2]);
-    // let mut c11 = load(&c[row1][col + 2..col + 4]);
-    //
-    // for k in 0..l {
-    //     let b0 = load(&b[k][col..col + 2]);
-    //     let b1 = load(&b[k][col + 2..col + 4]);
-    //
-    //     let a0 = alpha * a[row0][k];
-    //     let a1 = alpha * a[row1][k];
-    //
-    //     c00 = fma(c00, b0, a0);
-    //     c01 = fma(c01, b1, a0);
-    //     c10 = fma(c10, b0, a1);
-    //     c11 = fma(c11, b1, a1);
-    // }
-    //
-    // store(&mut c[row0][col..], c00, c01);
-    // store(&mut c[row1][col..], c10, c11);
     let m = ai.i_end - ai.i_start;
     let l = ai.j_end - ai.j_start;
     let n = bi.j_end - bi.j_start;
-    for i in (0..m).step_by(2) {
+    let mut a_tile = [[0.; 4]; 4];
+    let mut b_tile = [[0.; 4]; 4];
+    let mut c_tile = [[0.; 4]; 4];
+    for i in (0..m).step_by(4) {
         let a_start = (ai.i_start + i) * ai.n + ai.j_start;
         let c_start = (ci.i_start + i) * ci.n + ci.j_start;
-        let [c_row1, c_row2] = c.data.get_disjoint_mut([
+        let c_rows = c.data.get_disjoint_mut([
             c_start..c_start + n,
-            c_start + ai.n..c_start + n + ai.n
+            c_start + ci.n..c_start + n + ci.n,
+            c_start + 2 * ci.n..c_start + n + 2 * ci.n,
+            c_start + 3 * ci.n..c_start + n + 3 * ci.n
         ]).unwrap();
-        // let c_row1 = &mut c.data[c_start..c_start + n];
-        // let c_row2 = &mut c.data[c_start + ai.n..c_start + n + ai.n];
-        // register blocking 2x4
-        let mut c00 = [0.; 2];
-        let mut c01 = [0.; 2];
-        let mut c10 = [0.; 2];
-        let mut c11 = [0.; 2];
-        for k in (0..l).step_by(2) {
-            let a_ik0 = [alpha * a.data[a_start + k],
-                         alpha * a.data[a_start + k + 1]];
-            let a_ik1 = [alpha * a.data[a_start + k + ai.n],
-                         alpha * a.data[a_start + k + ai.n + 1]];
+        // register blocking 4x4
+        for k in (0..l).step_by(4) {
+            // load a_tile
+            for m in 0..4 {
+                for n in 0..4 {
+                    a_tile[m][n] = alpha * a.data[a_start + k + n + m * ai.n]
+                }
+            }
+
             let b_start = (bi.i_start + k) * bi.n + bi.j_start;
             let b_row1 = &b.data[b_start..b_start + n];
             let b_row2 = &b.data[b_start + bi.n..b_start + n + bi.n];
+            let b_row3 = &b.data[b_start + 2 * bi.n..b_start + n + 2 * bi.n];
+            let b_row4 = &b.data[b_start + 3 * bi.n..b_start + n + 3 * bi.n];
+            let b_rows = [b_row1, b_row2, b_row3, b_row4];
 
-            let mut b00 = [0.; 2];
-            let mut b01 = [0.; 2];
-            let mut b10 = [0.; 2];
-            let mut b11 = [0.; 2];
-            let chunked = c_row1.chunks_mut(4)
-                                .zip(b_row1.chunks(4))
-                                .zip(c_row2.chunks_mut(4)
-                                .zip(b_row2.chunks(4)));
-            for ((c_0j, b_0j), (c_1j, b_1j)) in chunked {
-                // load
-                b00[0] = b_0j[0];
-                b00[1] = b_0j[1];
-                b01[0] = b_0j[2];
-                b01[1] = b_0j[3];
-                b10[0] = b_1j[0];
-                b10[1] = b_1j[1];
-                b11[0] = b_1j[2];
-                b11[1] = b_1j[3];
+            let row_length = c_rows[0].len();
+            for z in (0..row_length).step_by(4) {
+                // load b and c tiles
+                for m in 0..4 {
+                    for n in 0..4 {
+                        b_tile[m][n] = b_rows[m][z..z + 4][n];
+                        c_tile[m][n] = c_rows[m][z..z + 4][n];
+                    }
+                }
 
-                c00[0] = c_0j[0];
-                c00[1] = c_0j[1];
-                c01[0] = c_0j[2];
-                c01[1] = c_0j[3];
-                c10[0] = c_1j[0];
-                c10[1] = c_1j[1];
-                c11[0] = c_1j[2];
-                c11[1] = c_1j[3];
+                for m in 0..4 {
+                    for n in 0..4 {
+                        for l in 0..4 {
+                            c_tile[m][n] = a_tile[m][l].mul_add(b_tile[l][n], c_tile[m][n]);
+                        }
+                    }
+                }
 
-                // gemm microkernel
-                c00[0] = a_ik0[0].mul_add(b00[0], c00[0]);
-                c00[0] = a_ik0[1].mul_add(b10[0], c00[0]);
-                c00[1] = a_ik0[0].mul_add(b00[1], c00[1]);
-                c00[1] = a_ik0[1].mul_add(b10[1], c00[1]);
-
-                c01[0] = a_ik0[0].mul_add(b01[0], c01[0]);
-                c01[0] = a_ik0[1].mul_add(b11[0], c01[0]);
-                c01[1] = a_ik0[0].mul_add(b01[1], c01[1]);
-                c01[1] = a_ik0[1].mul_add(b11[1], c01[1]);
-
-                c10[0] = a_ik1[0].mul_add(b00[0], c10[0]);
-                c10[0] = a_ik1[1].mul_add(b10[0], c10[0]);
-                c10[1] = a_ik1[0].mul_add(b00[1], c10[1]);
-                c10[1] = a_ik1[1].mul_add(b10[1], c10[1]);
-
-                c11[0] = a_ik1[0].mul_add(b01[0], c11[0]);
-                c11[0] = a_ik1[1].mul_add(b11[0], c11[0]);
-                c11[1] = a_ik1[0].mul_add(b01[1], c11[1]);
-                c11[1] = a_ik1[1].mul_add(b11[1], c11[1]);
-
-                // store
-                c_0j[0] = c00[0];
-                c_0j[1] = c00[1];
-                c_0j[2] = c01[0];
-                c_0j[3] = c01[1];
-                c_1j[0] = c10[0];
-                c_1j[1] = c10[1];
-                c_1j[2] = c11[0];
-                c_1j[3] = c11[1];
+                // store c tile
+                for m in 0..4 {
+                    for n in 0..4 {
+                        c_rows[m][n] = c_tile[m][n];
+                    }
+                }
             }
         }
     };
@@ -226,7 +148,6 @@ fn gemm_divide_and_conquer<'a>(
     b: &'a Matrix,
     c: &'a mut Matrix,
     alpha: f64,
-    beta: f64
 ) {
     let m = ci.i_end - ci.i_start;
     let l = bi.i_end - bi.i_start;
@@ -238,14 +159,14 @@ fn gemm_divide_and_conquer<'a>(
         let [b00, b01, b10, b11] = bi.quadrants();
         let [c00, c01, c10, c11] = ci.quadrants();
 
-        gemm_divide_and_conquer(a00, b00, c00, a, b, c, alpha, beta);
-        gemm_divide_and_conquer(a01, b10, c00, a, b, c, alpha, beta);
-        gemm_divide_and_conquer(a11, b10, c10, a, b, c, alpha, beta);
-        gemm_divide_and_conquer(a10, b00, c10, a, b, c, alpha, beta);
-        gemm_divide_and_conquer(a01, b11, c01, a, b, c, alpha, beta);
-        gemm_divide_and_conquer(a00, b01, c01, a, b, c, alpha, beta);
-        gemm_divide_and_conquer(a10, b01, c11, a, b, c, alpha, beta);
-        gemm_divide_and_conquer(a11, b11, c11, a, b, c, alpha, beta);
+        gemm_divide_and_conquer(a00, b00, c00, a, b, c, alpha);
+        gemm_divide_and_conquer(a01, b10, c00, a, b, c, alpha);
+        gemm_divide_and_conquer(a11, b10, c10, a, b, c, alpha);
+        gemm_divide_and_conquer(a10, b00, c10, a, b, c, alpha);
+        gemm_divide_and_conquer(a01, b11, c01, a, b, c, alpha);
+        gemm_divide_and_conquer(a00, b01, c01, a, b, c, alpha);
+        gemm_divide_and_conquer(a10, b01, c11, a, b, c, alpha);
+        gemm_divide_and_conquer(a11, b11, c11, a, b, c, alpha);
     }
 }
 
@@ -270,13 +191,15 @@ fn gemm<'a>(
     for c_i in c.data.iter_mut() {
         *c_i *= beta;
     }
-    gemm_divide_and_conquer(ai, bi, ci, a, b, c, alpha, beta);
+    gemm_divide_and_conquer(ai, bi, ci, a, b, c, alpha);
 }
 
 fn main() {
     let n = 4096;
-    let a = Matrix { nrow: n, ncol: n, data: vec![2.; n * n] };
-    let b = Matrix { nrow: n, ncol: n, data: vec![3.; n * n] };
+    let a_data = (0..4096 * 4096).map(|x| x as f64 / (2048. * 2048.)).collect();
+    let b_data = (0..4096 * 4096).rev().map(|x| x as f64 / (2048. * 2048.) - 0.5).collect();
+    let a = Matrix { nrow: n, ncol: n, data: a_data };
+    let b = Matrix { nrow: n, ncol: n, data: b_data };
     let mut c = Matrix { nrow: n, ncol: n, data: vec![0.1; n * n] };
     let alpha = 1.;
     let beta = 1.;
@@ -284,7 +207,9 @@ fn main() {
     let start_time = Instant::now();
     gemm(&a, &b, &mut c, alpha, beta);
     let end_time = Instant::now();
-    println!("{:?}", c.data[0]);
-    assert!(c.data.iter().all(|x| (x - 24576.1).abs() < 1e-8));
     println!("{:?}", end_time - start_time);
+    println!("{}", c.data[0]);
+    println!("{}", c.data[1413999]);
+    assert!((c.data[0] - 1.7669101716252045).abs() < 1e-8);
+    assert!((c.data[1413999] - 2072.150783736631).abs() < 1e-8);
 }
